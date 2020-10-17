18f63fef151b3f0ae9fc0d358837ff6363c5770f
daniellelshen <daniellelshen@gmail.com>
2020-10-16 19:08:20 -0700

    reworked authentication

--- HW3 SMTP Lab/mailClient.py
+++ HW3 SMTP Lab/mailClient.py
@@ -1,132 +1,103 @@
+from getpass import getpass
 from socket import *
 import base64
-import time
 import ssl
-from base64 import *
-from getpass import getpass
+import time
 
 
 def main():
 
-    yourEmail = "daniellelshen@gmail.com"
-    print("get password")
-
-    yourPass = getpass("Enter your email password: ")
-    print("got password")
-
-    msg = "\r\n CMPE 148: I love computer networks!"
+    msg = "\r\n  CMPE 148 Lab 3: I love computer networks!"
     endmsg = "\r\n.\r\n"
     # Choose a mail server (e.g. Google mail server) and call it mailserver
     mailserver = ("smtp.gmail.com", 587)  # Fill in start #Fill in end
 
     # Create socket called clientSocket and establish a TCP connection with mailserver
-    # Fill in start
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect(mailserver)
-    # Fill in end
 
-    recv = clientSocket.recv(1024).decode()
-    print(recv)
+
+    recv = clientSocket.recv(1024)
+    recv = recv.decode()
+    print("Message after connection request:" + recv)
     if recv[:3] != '220':
         print('220 reply not received from server.')
-
     # Send HELO command and print server response.
-    heloCommand = 'HELO Alice\r\n'
+    heloCommand = 'EHLO Microsoft\r\n'
     clientSocket.send(heloCommand.encode())
-    recv1 = clientSocket.recv(1024).decode()
-    print(recv1)
+    recv1 = clientSocket.recv(1024)
+    recv1 = recv1.decode()
+    print("Message after EHLO command:" + recv1)
     if recv1[:3] != '250':
         print('250 reply not received from server.')
 
-    # Sign into account to send email
-    strtlscmd = "STARTTLS\r\n".encode()
-    clientSocket.send(strtlscmd)
-    recv2 = clientSocket.recv(1024)
+    tls = "STARTTLS\r\n"
+    clientSocket.send(tls.encode())
+    recv = clientSocket.recv(1024)
+    print("TLS: " + recv.decode())
+    clientSocket = ssl.wrap_socket(clientSocket)
 
-    sslClientSocket = ssl.wrap_socket(clientSocket)
+    clientSocket.send(heloCommand.encode())
+    recv1 = clientSocket.recv(1024)
+    recv1 = recv1.decode()
+    print("Message after EHLO command:" + recv1)
 
-    emailAddress = b64encode(yourEmail.encode())
-    emailPassword = b64encode(yourPass.encode())
-
-    authorizationcmd = "AUTH LOGIN\r\n"
-
-    sslClientSocket.send(authorizationcmd.encode())
-    recv2 = sslClientSocket.recv(1024)
-    print(recv2)
-
-    sslClientSocket.send(emailAddress + "\r\n".encode())
-    recv3 = sslClientSocket.recv(1024)
-    print(recv3)
-
-    sslClientSocket.send(emailPassword + "\r\n".encode())
-    recv4 = sslClientSocket.recv(1024)
-    print(recv4)
+    # Info for username and password
+    # Authortization
+    username = input("Please enter your gmail: ")
+    password = input("Please enter password: ")
+    base64_str = ("\x00" + username + "\x00" + password).encode()
+    base64_str = base64.b64encode(base64_str)
+    base64_str = base64_str.strip("\n".encode())
+    authMsg = "AUTH PLAIN ".encode() + base64_str + "\r\n".encode()
+    clientSocket.send(authMsg)
+    recv_auth = clientSocket.recv(1024)
+    print("After auth: " + recv_auth.decode())
 
     # Send MAIL FROM command and print server response.
-    # Fill in start
-    mailFrom = "MAIL FROM: <{}>\r\n".format(yourEmail)
+    mailFrom = "MAIL FROM: <{}>\r\n".format(username)
     clientSocket.send(mailFrom.encode())
-    recv5 = sslClientSocket.recv(1024)
-    print("After MAIL FROM command: " + recv5)
-    if recv5[:3] != '250':
-        print('250 reply not received from server.')
-    # Fill in end
+    recv2 = clientSocket.recv(1024)
+    recv2 = recv2.decode()
+    print("After MAIL FROM command: " + recv2)
 
     # Send RCPT TO command and print server response.
-    # Fill in start
-    rcptTo = "RCPT TO:  <{}>\r\n".format(yourEmail)
+    rcptTo = "RCPT TO: <{}>\r\n".format(username)
     clientSocket.send(rcptTo.encode())
-    recv6 = clientSocket.recv(1024).decode()
-    print("After RCPT TO command: " + recv6)
-    if recv6[:3] != '250':
-        print('250 reply not received from server.')
-    # Fill in end
+    rcptTo = "RCPT TO: <{}>\r\n".format(username)
+    clientSocket.send(rcptTo.encode())
+    recv3 = clientSocket.recv(1024)
+    recv3 = recv3.decode()
+    print("After RCPT TO command: " + recv3)
+
 
     # Send DATA command and print server response.
-    # Fill in start
     data = "DATA\r\n"
-    sslClientSocket.send(data.encode())
-    recv7 = sslClientSocket.recv(1024)
-    print("After DATA command: " + recv7)
-    if recv7[:3] != '250':
-        print('250 reply not received from server.')
+    clientSocket.send(data.encode())
+    recv4 = clientSocket.recv(1024)
+    recv4 = recv4.decode()
+    print("After DATA command: " + recv4)
     # Fill in end
 
     # Send message data.
-    # Fill in start
-    subject = "Subject: CMPE 148 SMTP mail client testing \r\n\r\n"
-    message = msg
-    sslClientSocket.send("Subject: {}\n\n{}".format(subject, msg).encode())
-
-    clientSocket.send(message.encode())
-    clientSocket.send(endmsg.encode())
-    recv_msg = clientSocket.recv(1024).decode()
-    print("Response after sending message body:" + recv_msg)
-    if recv_msg[:3] != '250':
-        print('250 reply not received from server.')
-    # Fill in end
+    subject = "Subject: CMPE148 SMTP Lab \r\n\r\n"
+    clientSocket.send(subject.encode())
+    date = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
+    date = date + "\r\n\r\n"
+    clientSocket.send(date.encode())
+    clientSocket.send(msg.encode())
 
     # Message ends with a single period.
-    # Fill in start
-    sslClientSocket.send(endmsg.encode())
-    recv8 = sslClientSocket.recv(1024)
-    print("Response after sending message body:" + recv8)
-    if recv8[:3] != '250':
-        print('250 reply not received from server.')
-    # Fill in end
+    clientSocket.send(endmsg.encode())
+    recv_msg = clientSocket.recv(1024)
+    print("Response after sending message body:" + recv_msg.decode())
 
     # Send QUIT command and get server response.
-    # Fill in start
-    quitCmnd = 'QUIT\r\n'
-    sslClientSocket.send(quitCmnd.encode())
-    recv9 = sslClientSocket.recv(1024)
-    print("Response after sending quit command:" + recv9)
-    if recv9[:3] != '250':
-        print('250 reply not received from server.')
-
-    sslClientSocket.close()
-    print('Was successful!')
-    # Fill in end
+    quit = "QUIT\r\n"
+    clientSocket.send(quit.encode())
+    recv5 = clientSocket.recv(1024)
+    print(recv5.decode())
+    clientSocket.close()
 
 # main code to run
 if __name__ == "__main__":
