37e271ff22ffbd655e00a5e67ea9ff49e2e5f929
daniellelshen <daniellelshen@gmail.com>
2020-10-16 18:13:05 -0700

    initial creation of mailClient
    initial commit of HW 3 along with small import edits and definition of mailServer and clientSocket

---
+++ HW3 SMTP Lab/mailClient.py
@@ -0,0 +1,44 @@
+from socket import *
+import base64
+import time
+import ssl
+
+msg = "\r\n I love computer networks!"
+endmsg = "\r\n.\r\n"
+# Choose a mail server (e.g. Google mail server) and call it mailserver
+mailserver =  ("smtp.gmail.com", 587)# Fill in start #Fill in end
+# Create socket called clientSocket and establish a TCP connection with mailserver
+# Fill in start
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect(mailserver)
+# Fill in end
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[:3] != '220':
+    print('220 reply not received from server.')
+# Send HELO command and print server response.
+heloCommand = 'HELO Alice\r\n'
+clientSocket.send(heloCommand.encode())
+recv1 = clientSocket.recv(1024).decode()
+print(recv1)
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
+
+# Send MAIL FROM command and print server response.
+# Fill in start
+# Fill in end
+# Send RCPT TO command and print server response.
+# Fill in start
+# Fill in end
+# Send DATA command and print server response.
+# Fill in start
+# Fill in end
+# Send message data.
+# Fill in start
+# Fill in end
+# Message ends with a single period.
+# Fill in start
+# Fill in end
+# Send QUIT command and get server response.
+# Fill in start
+# Fill in end
