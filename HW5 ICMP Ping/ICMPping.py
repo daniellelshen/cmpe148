bd311d5aca5c22fdcafb772f188d4a2ed50f6a8d
daniellelshen <daniellelshen@gmail.com>
2020-11-14 14:38:58 -0800

    fixing it up

--- HW5 ICMP Ping/ICMPping.py
+++ HW5 ICMP Ping/ICMPping.py
@@ -1,51 +1,54 @@
 from socket import *
-
 import os
-
 import sys
-
 import struct
-
 import time
-
 import select
-
 import binascii
 
 ICMP_ECHO_REQUEST = 8
 
 
-def checksum(str):
+'''
+def checksum(string):
     csum = 0
-
-    countTo = (len(str) / 2) * 2
-
+    countTo = (len(string) // 2) * 2
     count = 0
-
     while count < countTo:
-        thisVal = ord(str[count + 1]) * 256 + ord(str[count])
+        thisVal = (string[count + 1]) * 256 + (string[count])
+        csum += thisVal
+        csum &= 0xffffffff
+        count += 2
+    if countTo < len(string):
+        csum += (string[len(string) - 1])
+        csum &= 0xffffffff
+    csum = (csum >> 16) + (csum & 0xffff)
+    csum = csum + (csum >> 16)
+    answer = ~csum
+    answer = answer & 0xffff
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+    return answer
+'''
+def checksum(str_):
+    # In this function we make the checksum of our packet
+    str_ = bytearray(str_)
+    csum = 0
+    countTo = (len(str_) // 2) * 2
 
+    for count in range(0, countTo, 2):
+        thisVal = str_[count+1] * 256 + str_[count]
         csum = csum + thisVal
-
         csum = csum & 0xffffffff
 
-        count = count + 2
-
-    if countTo < len(str):
-        csum = csum + ord(str[len(str) - 1])
-
+    if countTo < len(str_):
+        csum = csum + str_[-1]
         csum = csum & 0xffffffff
 
     csum = (csum >> 16) + (csum & 0xffff)
-
     csum = csum + (csum >> 16)
-
     answer = ~csum
-
     answer = answer & 0xffff
-
     answer = answer >> 8 | (answer << 8 & 0xff00)
-
     return answer
 
 
@@ -53,82 +56,97 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
     timeLeft = timeout
 
     while 1:
-
         startedSelect = time.time()
-
         whatReady = select.select([mySocket], [], [], timeLeft)
-
         howLongInSelect = (time.time() - startedSelect)
-
         if whatReady[0] == []:  # Timeout
-
             return "Request timed out."
 
         timeReceived = time.time()
-
         recPacket, addr = mySocket.recvfrom(1024)
 
         # Fill in start
 
+        #First, note that the ICMP header starts after bit 160.
+        #We want to know at which byte index to start at -
+        #there are 8 bits in 1 byte so we start at index 20
+        #The type = 1 byte, code = 1 byte, checksum = 2 bytes,
+        #ID = 2 bytes, and sequence = 2 bytes which gives us
+        #8 bytes total. So we want indices 20 - 28
+
         # Fetch the ICMP header from the IP packet
+        icmp_header = recPacket[20:28]
+
+        #We know to unpack in this order - look at how the ICMP header is packed in the sendOnePing method
+        icmp_type, icmp_code, icmp_checksum, icmp_id, icmp_seq = struct.unpack("bbHHh", icmp_header)
+
+        if type != 8 and icmp_id == ID:
+            bytesInDouble = struct.calcsize("d")
+            timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
+            return timeReceived - timeSent
 
         # Fill in end
-
+        timeLeft = timeLeft - howLongInSelect
+        if timeLeft <= 0:
+            return "Request timed out."
 
 
 def sendOnePing(mySocket, destAddr, ID):
     # Header is type (8), code (8), checksum (16), id (16), sequence (16)
 
     myChecksum = 0
-
-    # Make a dummy header with a 0 checksum.
-
+    # Make a dummy header with a 0 checksum
     # struct -- Interpret strings as packed binary data
-
-
-
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+    data = struct.pack("d", time.time())
     # Calculate the checksum on the data and the dummy header.
-
-
+    myChecksum = checksum(header + data)
 
     # Get the right checksum, and put in the header
 
+    if sys.platform == 'darwin':
+        # Convert 16-bit integers from host to network  byte order
+        myChecksum = htons(myChecksum) & 0xffff
+    else:
+        myChecksum = htons(myChecksum)
 
 
-        # Convert 16-bit integers from host to network byte order.
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+    packet = header + data
 
-  # AF_INET address must be tuple, not str
+    mySocket.sendto(packet, (destAddr, 1))  # AF_INET address must be tuple, not str
+
 
     # Both LISTS and TUPLES consist of a number of objects
-
-    # which can be referenced by their position number within the object
-
+    # which can be referenced by their position number within the object.
 
 def doOnePing(destAddr, timeout):
-    icmp = socket.getprotobyname("icmp")
-
-    # SOCK_RAW is a powerful socket type. For more details see: http://sock-raw.org/papers/sock_raw
-
-    # Fill in start
-
-    # Create Socket here
-
-    # Fill in end
+    icmp = getprotobyname("icmp")
 
 
+    # SOCK_RAW is a powerful socket type. For more details:   http://sockraw.org/papers/sock_raw
+    mySocket = socket(AF_INET, SOCK_RAW, icmp)
+
+    myID = os.getpid() & 0xFFFF  # Return the current process i
+    sendOnePing(mySocket, destAddr, myID)
+    delay = receiveOnePing(mySocket, myID, timeout, destAddr)
+    mySocket.close()
     return delay
 
 
 def ping(host, timeout=1):
-    # timeout=1 means: If one second goes by without a reply from the server,
+    # timeout=1 means: If one second goes by without a reply from the server,  	# the client assumes that either the client's ping or the server's pong is lost
+    dest = gethostbyname(host)
 
-    # the client assumes that either the client’s ping or the server’s pong is lost
-
-
+    #print("")
     # Send ping requests to a server separated by approximately one second
+    while 1:
+        delay = doOnePing(dest, timeout)
+        print("Pinging " + dest + " using Python with Delay:")
+        print(delay)
+        time.sleep(1)  # one second
+    return delay
 
 
-
-ping("www.poly.edu")
-
-
+ping("google.com")
+0
